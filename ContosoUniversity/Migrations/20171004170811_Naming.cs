286fcc549802dcc0a1b86c389ba9eb916b0bab83
Joao caselli <joaocaselli@hotmail.com>
2017-10-05 13:36:39 +0100

    With Packages

---
+++ ContosoUniversity/Migrations/20171004170811_Naming.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ContosoUniversity.Migrations
+{
+    public partial class Naming : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Enrollments",
+                table: "Student");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Enrollments",
+                table: "Student",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
---
+++ ContosoUniversity/Models/Entities/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContosoUniversity.Models.Entities
+{
+    public partial class Course
+    {
+        public Course()
+        {
+            Enrollments = new HashSet<Enrollment>();
+        }
+
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
