177f2a4c75300c6abe91697235f96cf9f8bce0bb
Joao caselli <joaocaselli@hotmail.com>
2017-10-17 17:59:15 +0100

    End of Sorting and Grouping

--- ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -1,25 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using ContosoUniversity.Models;
+using ContosoUniversity.Models.Entities;
+using ContosoUniversity.Models.SchoolViewModels;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace ContosoUniversity.Controllers
 {
     public class HomeController : Controller
     {
+        private ContosoUniversityContext _context;
+
+        public HomeController(ContosoUniversityContext ctx)
+        {
+            _context = ctx;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public async Task<ActionResult> About()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+            IQueryable<EnrollmentDateGroup> data =
+        from student in _context.Student
+        group student by student.EnrollmentDate into dateGroup
+        select new EnrollmentDateGroup()
+        {
+            EnrollmentDate = dateGroup.Key,
+            StudentCount = dateGroup.Count()
+        };
+            return View(await data.AsNoTracking().ToListAsync());
         }
 
         public IActionResult Contact()
