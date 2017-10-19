a7e85b94ad6193ab73bcf2c995ffe137e6276847
Joao caselli <joaocaselli@hotmail.com>
2017-10-19 16:17:01 +0100

    Student updated

--- ContosoUniversity/Models/Entities/Student.cs
+++ ContosoUniversity/Models/Entities/Student.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContosoUniversity.Models.Entities
 {
@@ -13,8 +14,12 @@ namespace ContosoUniversity.Models.Entities
 
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [StringLength(50)]
         public string LastName { get; set; }
+        [StringLength(50, ErrorMessage = "First Name cant be more than 50 characters")]
         public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
 
         public ICollection<Enrollment> Enrollment { get; set; }
